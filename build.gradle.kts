6a68f030712a6e3dda2d66012a3e8493893c9cc9
aesperer <piyrw9754@gmail.com>
2023-04-10 09:44:15 +0900

    :arrow_up: :: buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
 
 plugins {
-	id("org.springframework.boot") version "2.7.10"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+	id("org.springframework.boot") version PluginVersions.SPRING_BOOT_VERSION
+	id("io.spring.dependency-management") version PluginVersions.DEPENDENCY_MANAGER_VERSION
+	kotlin("jvm") version PluginVersions.JVM_VERSION
+	kotlin("plugin.spring") version PluginVersions.SPRING_PLUGIN_VERSION
 
 	`kotlin-dsl`
-}
-
-repositories {
-	jcenter()
 }
 
 group = "com.example"
@@ -22,10 +19,15 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation(Dependencies.SPRING_VALIDATION)
+	implementation(Dependencies.SPRING_SECURITY)
+	implementation(Dependencies.JWT)
+	implementation(Dependencies.AWS_SES)
+	implementation(Dependencies.SPRING_AWS)
+	annotationProcessor(Dependencies.CONFIGURATION_PROCESSOR)
+	implementation(Dependencies.OPEN_FEIGN)
+	implementation(Dependencies.GSON)
+	implementation(Dependencies.JACKSON_TYPE)
 }
 
 tasks.withType<KotlinCompile> {
