ec5450a48e202bd4816baa2e7e61692bcb578d0e
aesperer <piyrw9754@gmail.com>
2023-04-21 09:18:45 +0900

    :sparkles: :: error handler

---
+++ hexamodule-v2-infrastructure/src/main/kotlin/com/example/global/error/GlobalErrorHandler.kt
@@ -0,0 +1,15 @@
+package com.example.global.error
+
+import org.springframework.http.HttpStatus
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.net.BindException
+
+@RestControllerAdvice
+class GlobalErrorHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException::class)
+    protected fun handleBindException(e: BindingResult): ValidationErrorResponse? = ErrorResponse.of(e)
+}
