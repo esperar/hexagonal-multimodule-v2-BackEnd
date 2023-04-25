02bb4fd48854ce339c485da0f18b9865a5acf878
aesperer <piyrw9754@gmail.com>
2023-04-25 20:26:46 +0900

    :sparkles: :: security properties, scan

---
+++ hexamodule-v2-infrastructure/src/main/kotlin/com/example/global/config/PropertiesScanConfig.kt
@@ -0,0 +1,14 @@
+package com.example.global.config
+
+import com.example.global.security.properties.SecurityProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@ConfigurationPropertiesScan(
+    basePackageClasses = [
+        SecurityProperties::class
+    ]
+)
+@Configuration
+class PropertiesScanConfig {
+}
