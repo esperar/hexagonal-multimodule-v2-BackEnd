a9657cdda5e4562bd128616beb0b6477bfdd66aa
aesperer <piyrw9754@gmail.com>
2023-04-25 20:57:19 +0900

    :art: :: user persistence adapter

--- hexamodule-v2-persistence/src/main/kotlin/com/example/persistence/user/UserPersistenceAdapter.kt
+++ hexamodule-v2-persistence/src/main/kotlin/com/example/persistence/user/UserPersistenceAdapter.kt
@@ -1,10 +1,24 @@
 package com.example.persistence.user
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.example.domain.user.model.User
+import com.example.domain.user.spi.UserPort
+import com.example.persistence.user.mapper.UserMapper
+import com.example.persistence.user.repository.UserJpaRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
+import java.util.*
 
 @Component
 class UserPersistenceAdapter(
-    private val userMapper: UserMapper
-) {
+    private val userMapper: UserMapper,
+    private val userJpaRepository: UserJpaRepository
+) : UserPort {
+    override fun queryUserById(userId: UUID): User? = userMapper.toDomain(
+        userJpaRepository.findByIdOrNull(userId)
+    )
+
+    override fun existsUserById(userId: UUID): Boolean =
+        userJpaRepository.existsById(userId)
+
+
 }
