ab228bb2d1a5c0173c538abef4d4391357d764e3
aesperer <piyrw9754@gmail.com>
2023-04-26 21:15:27 +0900

    create security;

--- hexamodule-v2-infrastructure/src/main/kotlin/com/example/global/security/SecurityConfig.kt
+++ hexamodule-v2-infrastructure/src/main/kotlin/com/example/global/security/SecurityConfig.kt
@@ -1,12 +1,49 @@
 package com.example.global.security
 
+import com.example.global.config.FilterConfig
+import com.example.global.security.handler.CustomAccessDeniedHandler
+import com.example.global.security.handler.CustomAuthenticationEntryPoint
+import com.example.global.security.token.JwtParser
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtParser: JwtParser,
+    private val objectMapper: ObjectMapper,
+    private val customAuthenticationEntryPoint: CustomAuthenticationEntryPoint,
+    private val customAccessDeniedHandler: CustomAccessDeniedHandler
+) {
 
     @Bean
     protected fun passwordEncoder() = BCryptPasswordEncoder()
+
+    @Bean
+    protected fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf().disable()
+            .cors().and()
+
+        http
+            .sessionManagement()
+            .sessionCreationPolicy((SessionCreationPolicy.STATELESS))
+        http
+            .authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/").permitAll()
+            .anyRequest().denyAll()
+
+        http
+            .apply(FilterConfig(jwtParser, objectMapper))
+        http.exceptionHandling()
+            .authenticationEntryPoint(customAuthenticationEntryPoint)
+            .accessDeniedHandler(customAccessDeniedHandler)
+
+        return http.build()
+    }
 }
